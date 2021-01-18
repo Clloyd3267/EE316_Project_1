2cbe460b7ef1db03a545e1d1b2e16a63a87082a6
Clloyd3267 <37284048+Clloyd3267@users.noreply.github.com>
2021-01-18 10:34:13 -0500

    Added edge detector module and cleaned up code

--- keypad_testing/keypad_driver.vhd
+++ keypad_testing/keypad_driver.vhd
@@ -9,8 +9,6 @@
 --                of a 16 button keypad.
 --------------------------------------------------------------------------------
 -- TODO:
--- -> Add constant for scan time
--- -> Add edge triggering and is button pressed logic
 -- -> Test for multiple keys
 
 -----------------
@@ -20,19 +18,23 @@ library ieee;
   use ieee.std_logic_1164.all;
   use ieee.numeric_std.all;
 
+library work;
+  use work.edge_detector_utilities.all;
+
 --------------
 --  Entity  --
 --------------
 entity keypad_driver is
 generic
 (
-  C_CLK_FREQ_MHZ   : integer := 50;  -- System clock frequency in MHz
-  C_STABLE_TIME_MS : integer := 10;  -- Time required for button to remain stable in ms
-  C_SCAN_TIME_US   : integer := 2;   -- Time required for column power to fully settle in us
+  C_CLK_FREQ_MHZ   : integer     := 50;      -- System clock frequency in MHz
+  C_STABLE_TIME_MS : integer     := 10;      -- Time required for button to remain stable in ms
+  C_SCAN_TIME_US   : integer     := 2;       -- Time required for column power to fully settle in us
+  C_TRIGGER_EDGE   : t_EDGE_TYPE := RISING;  -- Edge to trigger on
 
   -- Dimensions of matrix keypad
-  C_NUM_ROWS       : integer := 4;
-  C_NUM_COLS       : integer := 4
+  C_NUM_ROWS       : integer     := 4;
+  C_NUM_COLS       : integer     := 4
 );
 port
 (
@@ -45,7 +47,7 @@ port
   -- Keypad Outputs (cols)
   O_KEYPAD_COLS    : out std_logic_vector((C_NUM_COLS-1) downto 0);
 
-  -- Binary representation of keypad state
+  -- Final binary representation of keypad state
   O_KEYPAD_BINARY  : out std_logic_vector(((C_NUM_ROWS * C_NUM_COLS)-1) downto 0)
 );
 end entity keypad_driver;
@@ -61,8 +63,8 @@ architecture rtl of keypad_driver is
   component debounce_button is
   generic
   (
-    C_CLK_FREQ_MHZ   : integer := 50;  -- System clock frequency in MHz
-    C_STABLE_TIME_MS : integer := 10   -- Time required for button to remain stable in ms
+    C_CLK_FREQ_MHZ   : integer;       -- System clock frequency in MHz
+    C_STABLE_TIME_MS : integer        -- Time required for button to remain stable in ms
   );
   port
   (
@@ -72,6 +74,21 @@ architecture rtl of keypad_driver is
     O_BUTTON         : out std_logic  -- Debounced button data
   );
   end component debounce_button;
+
+  component edge_detector is
+  generic
+  (
+    C_CLK_FREQ_MHZ   : integer;       -- System clock frequency in MHz
+    C_TRIGGER_EDGE   : t_EDGE_TYPE    -- Edge to trigger on
+  );
+  port
+  (
+    I_CLK            : in std_logic;  -- System clk frequency of (C_CLK_FREQ_MHZ)
+    I_RESET_N        : in std_logic;  -- System reset (active low)
+    I_SIGNAL         : in std_logic;  -- Input signal to pass through edge detector
+    O_EDGE_SIGNAL    : out std_logic  -- Output pulse on (C_TRIGGER_EDGE) edge
+  );
+  end component edge_detector;
 
   -------------
   -- SIGNALS --
@@ -91,10 +108,13 @@ architecture rtl of keypad_driver is
   signal s_keypad_cols             : std_logic_vector((C_NUM_COLS-1) downto 0);
 
   -- Binary representation of keypad state (undebounced)
-  signal s_keypad_binary           : std_logic_vector(((C_NUM_ROWS * C_NUM_COLS)-1) downto 0);
+  signal s_keypad_binary_inital    : std_logic_vector(((C_NUM_ROWS * C_NUM_COLS)-1) downto 0);
 
   -- Binary representation of keypad state (debounced)
   signal s_keypad_binary_debounced : std_logic_vector(((C_NUM_ROWS * C_NUM_COLS)-1) downto 0);
+
+  -- Binary representation of keypad state (edge detection)
+  signal s_keypad_binary_edge      : std_logic_vector(((C_NUM_ROWS * C_NUM_COLS)-1) downto 0);
 
 begin
 
@@ -108,12 +128,29 @@ begin
       )
       port map
       (
-        I_CLK     => I_CLK,
-        I_RESET_N => I_RESET_N,
-        I_BUTTON  => s_keypad_binary(i),
-        O_BUTTON  => s_keypad_binary_debounced(i)
+        I_CLK            => I_CLK,
+        I_RESET_N        => I_RESET_N,
+        I_BUTTON         => s_keypad_binary_inital(i),
+        O_BUTTON         => s_keypad_binary_debounced(i)
       );
   end generate keypad_debounce;
+
+  -- Capture edges for keypad outputs
+  keypad_edge_detection: for i in 0 to ((C_NUM_ROWS * C_NUM_COLS)-1) generate
+    edge_detect_keys: edge_detector
+      generic map
+      (
+        C_CLK_FREQ_MHZ => C_CLK_FREQ_MHZ,
+        C_TRIGGER_EDGE => C_TRIGGER_EDGE
+      )
+      port map
+      (
+        I_CLK          => I_CLK,
+        I_RESET_N      => I_RESET_N,
+        I_SIGNAL       => s_keypad_binary_debounced(i),
+        O_EDGE_SIGNAL  => s_keypad_binary_edge(i)
+      );
+  end generate keypad_edge_detection;
 
   ------------------------------------------------------------------------------
   -- Process Name     : KEYPAD_EN_CNTR
@@ -179,7 +216,7 @@ begin
           when COL_POWER_STATE =>
               s_keypad_state <= COL_READ_STATE;
 
-          when COL_READ_STATE =>
+          when COL_READ_STATE  =>
               s_keypad_state <= COL_POWER_STATE;
 
               -- Counter logic to increment column
@@ -190,7 +227,7 @@ begin
               end if;
 
           -- Error condition, should never occur
-          when others =>
+          when others          =>
             s_keypad_state      <= IDLE_STATE;
             s_col_index_counter <= 0;
         end case;
@@ -204,9 +241,9 @@ begin
 
   ------------------------------------------------------------------------------
   -- Process Name     : KEYPAD_TO_BINARY
-  -- Sensitivity List : I_CLK           : System clock
-  --                    I_RESET_N       : System reset (active low logic)
-  -- Useful Outputs   : s_keypad_binary : Binary representation of keypad state
+  -- Sensitivity List : I_CLK                  : System clock
+  --                    I_RESET_N              : System reset (active low logic)
+  -- Useful Outputs   : s_keypad_binary_inital : Binary representation of raw keypad state
   -- Description      : Entity to control the powering and reading of the
   --                    keypad columns and rows based on the current s_keypad_state.
   --                    Outputs the current binary number of keypad (0-15)
@@ -214,8 +251,8 @@ begin
   KEYPAD_TO_BINARY: process (I_CLK, I_RESET_N)
   begin
     if (I_RESET_N = '0') then
-      s_keypad_cols   <= (others=>'0');
-      s_keypad_binary <= (others=>'0');
+      s_keypad_cols          <= (others=>'0');
+      s_keypad_binary_inital <= (others=>'0');
 
     elsif ((rising_edge(I_CLK))) then
 
@@ -231,10 +268,10 @@ begin
       -- Read row state
       if (s_keypad_state = COL_READ_STATE) then
         for row in 0 to (C_NUM_ROWS-1) loop
-          s_keypad_binary(row * s_col_index_counter) <= I_KEYPAD_ROWS(row);
+          s_keypad_binary_inital(row * s_col_index_counter) <= I_KEYPAD_ROWS(row);
         end loop;
       else
-        s_keypad_binary <= s_keypad_binary;
+        s_keypad_binary_inital <= s_keypad_binary_inital;
       end if;
     end if;
   end process KEYPAD_TO_BINARY;
@@ -242,6 +279,6 @@ begin
 
   -- Assign final outputs
   O_KEYPAD_COLS   <= s_keypad_cols;
-  O_KEYPAD_BINARY <= s_keypad_binary_debounced;
+  O_KEYPAD_BINARY <= s_keypad_binary_edge;
 
 end architecture rtl;
